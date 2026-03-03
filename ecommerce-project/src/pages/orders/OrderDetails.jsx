cac22d948d0a8fadf40ffa41323db089b0e67817
DND-new <derick.nathaniel.dizon@gmail.com>
2026-03-03 10:30:45 +0800

    Create add to cart feature in orders page

--- ecommerce-project/src/pages/orders/OrderDetails.jsx
+++ ecommerce-project/src/pages/orders/OrderDetails.jsx
@@ -1,12 +1,21 @@
+import axios from "axios";
 import dayjs from "dayjs";
 import { Link } from "react-router";
 import { Fragment } from "react";
 import BuyAgain from "../../assets/images/icons/buy-again.png";
 
-export function OrderDetails({ order, deliveryPercent }) {
+export function OrderDetails({ order, loadCart }) {
   return (
     <div className="order-details-grid">
       {order.products.map((orderProduct) => {
+        const addToCart = async () => {
+          await axios.post("api/cart-items", {
+            productId: orderProduct.product.id,
+            quantity: 1,
+          });
+          await loadCart();
+        };
+
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
@@ -23,7 +32,9 @@ export function OrderDetails({ order, deliveryPercent }) {
               </div>
               <button className="buy-again-button button-primary">
                 <img className="buy-again-icon" src={BuyAgain} />
-                <span className="buy-again-message">Add to Cart</span>
+                <span className="buy-again-message" onClick={addToCart}>
+                  Add to Cart
+                </span>
               </button>
             </div>
 
